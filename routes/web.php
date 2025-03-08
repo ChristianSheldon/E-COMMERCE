760d57c956fe58e69152a3fdedc2b0256f9c13c4
ChristianSheldon <christiansheldon@hotmail.com>
2025-03-08 12:18:20 -0500

    adds controllers and views with test data and routes them to routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'home'])->name('home');
+
+
+Route::get('/cart', [PagesController::class, 'cart'])->name('cart');
